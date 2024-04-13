3d8e79000df0afe1988be64d0bdfbf51ae0f6cf6
blozink <juavergara24@gmail.com>
2024-04-12 23:59:51 -0400

    ajuste de sonido

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,33 +1,34 @@
-// Definición de los sonidos utilizados en el juego
+
+
+// Carga los archivos de audio
 const backgroundMusic = new Audio('assets/audio/fondo.mp3');
 const victorySound = new Audio('assets/audio/victoria.mp3');
 const defeatSound = new Audio('assets/audio/derrota.mp3');
 const touchSound = new Audio('assets/audio/toque.mp3');
 
-// Configuración inicial para la música de fondo
-backgroundMusic.loop = true; // Configura la música de fondo para que se repita
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
     const playButton = document.getElementById('play-button');
     if (playButton) {
         playButton.addEventListener('click', function() {
-            playBackgroundMusic(); // Inicia la música de fondo cuando el jugador presiona el botón de jugar
-            restartGame(); // Inicia la secuencia del juego
+            
+           
+
+            // Reproduce el sonido de toque y redirecciona
+            touchSound.play();
+            setTimeout(function() {
+                window.location.href = 'juego.html';
+            }, 200); // Retraso para permitir que el sonido se reproduzca
         });
     }
 });
 
-function playBackgroundMusic() {
-    let playPromise = backgroundMusic.play();
 
-    if (playPromise !== undefined) {
-        playPromise.then(_ => {
-            console.log('Reproducción iniciada con éxito');
-        }).catch(error => {
-            console.error('Error al intentar reproducir la música de fondo:', error);
-        });
-    }
-}
+
+
+// Opcional: Configurar la música de fondo para que se repita
+backgroundMusic.loop = true;
+
 
 let cards = [
     'assets/img/A.png', 'assets/img/A.png',
@@ -39,7 +40,8 @@ let cards = [
 ];
 let selectedCards = [];
 let matchesFound = 0;
-let activeInterval;
+let gameTimeout;
+let memorizeTimeout;
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -75,13 +77,15 @@ function createBoard() {
     });
 }
 
+
 function flipCard() {
     if (selectedCards.length === 2 || this.classList.contains('flipped')) {
         return;
     }
 
-    touchSound.currentTime = 0;
-    touchSound.play();
+    touchSound.currentTime = 0; // Reinicia el audio a 0
+    touchSound.play(); // Reproduce el sonido de toque
+
     this.classList.add('flipped');
     selectedCards.push(this);
     
@@ -89,6 +93,10 @@ function flipCard() {
         checkForMatch();
     }
 }
+
+
+
+
 
 function checkForMatch() {
     const isMatch = selectedCards[0].getElementsByClassName('image-top')[0].style.backgroundImage ===
@@ -108,46 +116,10 @@ function checkForMatch() {
     }
 }
 
-function endGame(won) {
-    clearInterval(activeInterval); // Detiene cualquier temporizador activo
-    backgroundMusic.pause();
-    backgroundMusic.currentTime = 0;
-
-    if (won) {
-        victorySound.play();
-    } else {
-        defeatSound.play();
-    }
-
-    showEndGamePopup(won);
-}
-
-function showEndGamePopup(won) {
-    const modal = document.getElementById('endgame-modal');
-    const message = document.getElementById('endgame-message');
-    const playAgainButton = document.getElementById('play-again-button');
-
-    message.textContent = won ? '¡Felicidades, has ganado!' : 'Perdiste, vuelve a intentarlo.';
-    modal.style.display = "flex";
-
-    playAgainButton.onclick = function() {
-        modal.style.display = "none";
-        restartGame(); // Reinicia el juego
-    };
-}
-
-function restartGame() {
-    selectedCards = [];
-    matchesFound = 0;
-    createBoard(); // Re-crea el tablero
-    gameSequence(); // Reinicia la secuencia del juego
-}
-
-function gameSequence() {
-    startCountdown(30, 'Tiempo restante: ', () => endGame(false));
-}
+let activeInterval; // Referencia global para el intervalo activo
 
 function startCountdown(seconds, messagePrefix, callback) {
+    backgroundMusic.play();
     let counter = seconds;
     displayStatus(`${messagePrefix} `, counter);
     clearInterval(activeInterval); // Limpiar cualquier intervalo previo
@@ -161,15 +133,107 @@ function startCountdown(seconds, messagePrefix, callback) {
     }, 1000);
 }
 
+
+
+
+function gameSequence() {
+    startCountdown(5, 'El juego comenzará en', function() {
+        flipAllCards(true);
+        startCountdown(10, 'Tiempo para memorizar', function() {
+            flipAllCards(false);
+            startCountdown(20, 'Tiempo para jugar', function() {
+                endGame(false);
+            });
+        });
+    });
+}
+
+
+
+
+
+
+function flipAllCards(show) {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.classList.toggle('flipped', show);
+    });
+}
+
 function displayStatus(message, countdown) {
     const statusElement = document.getElementById('game-status');
     if (statusElement) {
-        statusElement.innerHTML = `${message} <span class="countdown-number">${countdown}</span> segundos`;
+        // Si countdown es un número, se mostrará el mensaje con el número resaltado.
+        if (typeof countdown === 'number') {
+            statusElement.innerHTML = `${message} <span class="countdown-number">${countdown}</span> segundos`;
+        } else {
+            statusElement.textContent = message;
+        }
     } else {
         console.error("No se encontró el elemento 'game-status'.");
     }
 }
 
+
+
+function endGame(won) {
+    clearInterval(activeInterval);
+    backgroundMusic.pause(); // Detiene la música de fondo
+    backgroundMusic.currentTime = 0; // Opcional: rebobina la música de fondo
+
+
+    if (won) {
+        victorySound.play(); // Reproduce el sonido de victoria
+    } else {
+        defeatSound.play(); // Reproduce el sonido de derrota
+    }
+
+    const message = won ? '¡Felicidades, has ganado!' : 'Perdiste, vuelve a intentarlo.';
+    displayStatus(message);
+    showEndGamePopup(won);
+
+}
+
+function playTouchSound() {
+    touchSound.currentTime = 0; // Reinicia el sonido
+    touchSound.play(); // Reproduce el sonido
+}
+
+
+
+function showEndGamePopup(won) {
+    const modal = document.getElementById('endgame-modal');
+    const message = document.getElementById('endgame-message');
+    const playAgainButton = document.getElementById('play-again-button'); // Asegúrate de que este es el ID correcto
+
+    message.textContent = won ? '¡Felicidades, has ganado!' : 'Perdiste, vuelve a intentarlo.';
+    modal.style.display = "flex";
+
+
+    playAgainButton.onclick = function() {
+        playTouchSound(); // Reproduce el sonido de toque
+        setTimeout(function() { // Agrega un pequeño retraso para permitir que el sonido se inicie antes de cambiar de página
+            window.location.href = 'index.html'; // Redirecciona al usuario a index.html
+        }, 200);
+    };
+}
+
+function restartGame() {
+    selectedCards = [];
+    matchesFound = 0;
+    clearInterval(activeInterval);
+    
+    // Detener todos los sonidos
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
+    victorySound.pause();
+    victorySound.currentTime = 0;
+    defeatSound.pause();
+    defeatSound.currentTime = 0;
+
+    createBoard();
+    gameSequence();
+}
 
 //Call this function at the beginning to set up event listeners for the modal
 function setupModal() {
